89861afbb639696b80fbaafa748f62958907e93c
MargaritaBelozerova <belozerovaonline@gmail.com>
2021-12-06 22:39:56 +0300

    написан код для расчета суммы в модальном окне (урок 16)

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 const btnCart = document.getElementById('btnCart');
 const btnClose = document.querySelector('.btn-close');
 const modal = document.querySelector('.modal');
+const total = document.querySelector('.modal-sum');
+let fullPrice;
+const rows = document.querySelectorAll('.row');
 
 const modalOpen = function() {
   modal.classList.toggle('modal--close');
@@ -13,7 +16,46 @@ const modalClick = function(event) {
   }
 }
 
+const incSum = function(item, i, arr) {
+  let sumBlock = item.querySelector('.price');
+  let sum = +sumBlock.textContent;
+  fullPrice += sum;
+}
+
+const getFullPrice = function() {
+  fullPrice = 0;
+  const rows = modal.querySelectorAll('.row');
+  rows.forEach(incSum);
+  total.textContent = fullPrice;
+}
+
+const setButtonClick = function(item, i, arr) {
+  let priceBlock = item.querySelector('.price');
+  let price = +priceBlock.textContent;
+  let сountBlock = item.querySelector('.count');
+  let count = +сountBlock.textContent;
+  let plusButton = item.querySelector('.plus-button');
+  let minusButton = item.querySelector('.minus-button');
+
+  plusButton.addEventListener('click', function() { 
+    count += 1;
+    сountBlock.textContent = count;
+    priceBlock.textContent = count * price;    
+    getFullPrice();
+  });
+  
+  minusButton.addEventListener('click', function() { 
+    if (count > 0) {
+      count -= 1;
+    }
+    сountBlock.textContent = count;
+    priceBlock.textContent = count * price;    
+    getFullPrice();
+  });
+}
+
 btnCart.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalOpen);
-modal.addEventListener('click', modalClick)
- 
+modal.addEventListener('click', modalClick);
+rows.forEach(setButtonClick);
+getFullPrice();
